d0315fdf6ebec3503ffd353cbbe20c8b35a20e5c
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-07-23 11:10:25 +0800

    Add Class

--- python_basic/test_print.py
+++ python_basic/test_print.py
@@ -1,2 +1,37 @@
-import socket
-#Search IP
+"""
+C
+L
+A
+S
+S
+"""
+class MiaClasse:
+   
+   Count = 0
+
+   def __init__(root, item, price, vendor):
+      root.item = item
+      root.price = price
+      root.vendor = vendor
+      MiaClasse.Count += 1
+   
+   def CountResult(root):
+     print MiaClasse.Count
+
+   def PrintData(root):
+      print "Item: ", root.item,  ", Price: ", root.price, ",Vendor:", root.vendor
+
+
+
+#Create Data
+
+D1 = MiaClasse("Apple", 10, "SuperMarket")
+D2 = MiaClasse("Lemon", 5, "My Farm")
+D3 = MiaClasse("PineApple", 50, "Market")
+
+#Print Data
+D1.PrintData()
+D2.PrintData()
+D3.PrintData()
+print "Total:", MiaClasse.Count
+
