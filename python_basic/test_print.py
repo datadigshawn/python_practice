02da2aaf568b4e65f161644c455d0f11279ce565
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-07-23 14:14:10 +0800

    add tuples

--- python_basic/test_print.py
+++ python_basic/test_print.py
@@ -70,3 +70,22 @@ def square(Val):
 three=3
 REL = square(three);
 print "Values After Square = ", REL
+
+"""
+T
+U
+P
+L
+E
+S
+"""
+T1=("Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct" , "Nov", "Dec")
+T2= ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+T=T1+T2
+print T
+
+#Index
+print T1[6]
+print T1[-2]
+print T1[5:9]
+print len(T2)
