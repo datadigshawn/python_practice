e29dedc5675c3dd02dd36785b57872fc29507047
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-07-23 11:31:15 +0800

    Add Python Def Function Practice

--- python_basic/test_print.py
+++ python_basic/test_print.py
@@ -35,3 +35,38 @@ D2.PrintData()
 D3.PrintData()
 print "Total:", MiaClasse.Count
 
+"""
+Def
+F
+U
+N
+C
+T
+I
+O
+N
+"""
+"""
+Calling Function
+"""
+def PrintFunc( str ):
+   print str;
+   return;
+
+#PrintFunc
+PrintFunc("Yo! Function ");
+PrintFunc("Print!!!!");
+
+"""
+Pass by Value
+"""
+#square Function Here!
+def square(Val):
+   SquareRel=Val*Val
+   print "Got Value and Square equal=", SquareRel
+   return SquareRel
+
+# Throw Value to square Function
+three=3
+REL = square(three);
+print "Values After Square = ", REL
