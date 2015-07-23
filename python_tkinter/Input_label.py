66b9a45a1f7791b062dacaba5ae43ebacc2d4b4e
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-07-24 01:33:53 +0800

    Update Input_label.py

--- python_tkinter/Input_label.py
+++ python_tkinter/Input_label.py
@@ -1,3 +1,18 @@
 import sys
 from tkinter import *
 def mhello():
+    mtext = meant.get()
+    mlabel2 = Label(mGui,text=text).pack()
+    return
+
+mGui = Tk()
+meant = StringVar()
+
+mGui.geometry(450*450+500+309)
+mGui.title('title')
+
+mlabel = Label(mGui,text = 'my label').pack()
+
+mbutton = Button(mGui,text = 'OK', command = mhello, fg = 'red', bg = 'blue').pack()
+
+mEntry = Entry(mGui, textvariable=ment).pack
