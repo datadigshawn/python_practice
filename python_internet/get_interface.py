ac76be36d0cce1116c83f6740fcd9ab851c48450
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-07-25 17:01:33 +0800

    Update get_interface.py

--- python_internet/get_interface.py
+++ python_internet/get_interface.py
@@ -1,3 +1,7 @@
 from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
 import netifaces as ni
-ni.interfaces()
+MyInterface = ni.interfaces()
+print MyInterface
+ni.ifaddresses('eth0')[AF_LINK]
+ni.ifaddresses('eth0')[AF_INET]
+ni.ifaddresses('eth0')[AF_INET][0]['addr']
