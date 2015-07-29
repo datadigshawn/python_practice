7c4d2c76a467b301d092af355d991df0b70b7147
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-07-29 17:32:08 +0800

    add comment

--- python_tkinter/frame_switch.py
+++ python_tkinter/frame_switch.py
@@ -9,17 +9,21 @@ class DestroyTest():
     def __init__(self, top):
         self.top=top
         self.top.geometry("+10+10")
+        #frame
         self.frame=tk.Frame(self.top)
         self.frame.grid()
+        #Label (In the frame)
         test_label=tk.Label(self.frame, text="Label")
         test_label.grid(row=1, column=0)
-        destroy_button=tk.Button(self.frame, text="Destroy Frame", \
-                                 command=self.destroy)
-        destroy_button.grid(row=10, column=0)
+        #Destroy Button (In the frame)
+        destroy_button=tk.Button(self.frame, text="Destroy Frame", command=self.destroy)
+        destroy_button.grid(row=20, column=5)
+        #Exit button  (In the top)
         exit_button=tk.Button(self.top, text="Exit", command=top.quit)
         exit_button.grid(row=10, column=0)
     def destroy(self):
         self.frame.destroy()
+        #Destroy n Popout new window
         self.new_toplevel=tk.Toplevel(self.top, takefocus=True)
         self.new_toplevel.geometry("+50+50")
         self.new_toplevel.grid()
