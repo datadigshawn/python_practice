e403dddf5a3b61320d6f9cdb03a6a4b3f1f95763
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-08-05 13:44:17 +0800

    get yesterday currency

--- python_internet/Mechanize_login.py
+++ python_internet/Mechanize_login.py
@@ -2,9 +2,10 @@ import mechanize
 from bs4 import BeautifulSoup
 import urllib2
 from ntlm import HTTPNtlmAuthHandler
-
-user = 'ECNT\\1848'
-password = "Marchisio8"
+from datetime import date, timedelta
+import time
+user = 'domain\\name'
+password = "*******"
 url = "http://www.ctci.com.tw/Acc_Rep/rate/rate.asp"
 passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
 passman.add_password(None, url, user, password)
@@ -32,5 +33,32 @@ response=urllib2.urlopen(req)
 #print(response.read())
 parser = response.read()
 soup = BeautifulSoup(parser, "html.parser")
-print soup
+#print soup
+
+#get today weekday
+
+yWeekday = date.today().weekday()
+
+wColor = ("#f4f5e7", "#eaf5ee")
+count = 0  #can fit yesterday
+
+for table in soup.findAll('table', align="left", border="0", cellpadding="3", cellspacing="0"):
+    count += 1
+    if count == yWeekday:
+        print table
+    """
+for target in soup.findAll('tr', {'bgcolor':True}):
+    #print target
+    #print target['bgcolor']
+    count += 1
+for target1 in soup.findAll('tr', {'bgcolor':False}, height="30px"):
+    #print target1
+    target1
+"""
+# maybe for 1 weak (color) 
+
+
+
+
+
 
