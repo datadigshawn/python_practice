842fa343763323ba2543062645a66ad9b80a6cd0
geek-ragazza <tecnologia.ragazza@gmail.com>
2015-08-11 14:30:53 +0800

    Update w_Price_HTML.py

--- project_python/scraping/w_Price_HTML.py
+++ project_python/scraping/w_Price_HTML.py
@@ -2,70 +2,95 @@
 import datetime
 import sqlite3 as lite
 import array
-import scraping
-
+import time
+import re
 #Scrape Data
-lme = scraping.ScrapingLMELogin()
-lme.LoginGetValue()
-lmeValues =  lme.LoginGetValue()
-sh = scraping.ScrapingSHCopper()
-sh.SHGetValue()
-shValues = sh.SHGetValue()
 
-#TIME
-DailyNow= datetime.datetime.today()
-print DailyNow 
-print DailyNow.year
-mm = '{:02d}'.format(DailyNow.month)
-dd = '{:02d}'.format(DailyNow.day)
-date_stamp = ("%s-%s-%s"%(DailyNow.year,mm,dd))
-print date_stamp
+
+
+
+
+#fetch last day (not today)
+today = datetime.date.today( ) 
+ytd = today - datetime.timedelta(days=1)
+ymm =  '{:02d}'.format(ytd.month)
+ydd =  '{:02d}'.format(ytd.day)
+divYdate = ("%s-%s-%s"%(ytd.year,ymm,ydd))
+print divYdate
+print today
 
 #fetch database n write to the html
-Hcon = lite.connect('daily_price.db')
-"""
-#get last two days (no weekend)
-monday -3,-4
-2 -1 -3
-(3,4,5) -1 -2
+#SQLite
+con = lite.connect('daily_price.db')
+with con:
+    cur=con.cursor()
+    getCount1=0
+    while getCount1 != 1:
+        sql_action='SELECT * FROM daily WHERE Record_Date=?'
+        parameters = [divYdate]
+        for row1 in con.execute(sql_action,parameters):
+            print "find", row1[1], ytd
+            #back a day!!! for next while loop
+            ytd2 =ytd- datetime.timedelta(days=1)
+            ymm2 =  '{:02d}'.format(ytd2.month)
+            ydd2 =  '{:02d}'.format(ytd2.day)
+            divYdate2 = ("%s-%s-%s"%(ytd2.year,ymm2,ydd2))
+            getCount1 =1
+            break
+        else:
+            ytd = ytd - datetime.timedelta(days=1)
+            ymm =  '{:02d}'.format(ytd.month)
+            ydd =  '{:02d}'.format(ytd.day)
+            divYdate = ("%s-%s-%s"%(ytd.year,ymm,ydd))
+            print ("NOT FOUND%s"%(divYdate))
+    getCount2=0
+    print divYdate2, "HAHA"
+    while getCount2 != 1:
+        sql_action='SELECT * FROM daily WHERE Record_Date=?'
+        parameters = [divYdate2]
+        for row2 in con.execute(sql_action,parameters):
+            print "find", row2[1]
+            getCount2 = 1
+            break
+        else:
+            ytd2 = ytd2 - datetime.timedelta(days=1)
+            ymm =  '{:02d}'.format(ytd2.month)
+            ydd =  '{:02d}'.format(ytd2.day)
+            divYdate2 = ("%s-%s-%s"%(ytd2.year,ymm,ydd))
+            print ("NOT FOUND%s"%(divYdate2))
 
-with Hcon:
-    cur=Hcon.cursor()
-    sql_action=''
-    parameters = [date_stamp]
-    Hcon.execute(sql_action,parameters):
-"""
-        
 
+print row1
+print row2
+date1=row1[1].encode("utf-8")
+date2=row2[1].encode("utf-8")
+#write data in html file
 f = open('price.html','w')
-date = "date"
-copper = "copper"
-al = "al"
-nick = "nick"
-zinc = "zinc"
+
 message = """<html>
 <head><meta charset="UTF-8"></head>
 <body style="font-family:'新細明體'; font-size:'24px'" >
 <p style="color:blue">各位長官好:</p>
-<p style="color:blue">LME國際金屬行情CASH BUYER價格(USD/TON):</p>
+<p style="color:blue">LME國際金屬行情CASH BUYER價格(USD/TON):</p>"""
+message1="""<p style="color:#FF3399">
+<a>%s&nbsp&nbsp&nbsp 銅</a><a>%s,&nbsp鋁%s,&nbsp鎳%s,&nbsp鋅%s</a></p>
 <p style="color:#FF3399">
-<a>%s&nbsp&nbsp&nbsp 銅</a><a>%s,鋁%s,鎳%s,鋅%s</a></p>
-<p style="color:#FF3399">
-<a>%s&nbsp&nbsp&nbsp 銅</a><a>%s,鋁%s,鎳%s,鋅%s</a></p>
-
+<a>%s&nbsp&nbsp&nbsp 銅</a><a>%s,&nbsp鋁%s,&nbsp鎳%s,&nbsp鋅%s</a></p>
 <p style="color:blue">滬銅行情價格(CNY/TON):</p>
 <p style="color:#FF3399">%s&nbsp&nbsp%s</p>
-
+<p style="color:#FF3399">%s&nbsp&nbsp%s</p>
+"""%(date1,row1[2],row1[3],row1[4],row1[5],date2,row2[2],row2[3],row2[4],row2[5],date1,row1[6],date2,row2[6])
+message2 = """
 <p style="color:blue">美國西德州原油價格(USD/桶):</p>
-<p style="color:#FF3399">%s&nbsp&nbsp 原油%s</p>
-<p style="color:#FF3399">%s&nbsp&nbsp 原油%s</p>
-
+<p style="color:#FF3399">%s&nbsp&nbsp 原油&nbsp%s</p>
+<p style="color:#FF3399">%s&nbsp&nbsp 原油&nbsp%s</p>
 <p style="color:blue">前日匯率(%s)</p>
 <p style="color:#FF3399">USD/TWD&nbsp&nbsp%s</p>
 <p style="color:#FF3399">CNY/TWD&nbsp&nbsp%s</p>
 <p style="color:#FF3399">JPY/TWD&nbsp&nbsp%s</p>
 <p style="color:#FF3399">EUR/TWD&nbsp&nbsp%s</p>
-<p style="color:blue">中鋼2015年第二季7月盤價:</p>
+"""%(date1,row1[7],date2,row2[7],date1,row1[8],row1[9],row1[10],row1[11])
+message3="""<p style="color:blue">中鋼2015年第二季7月盤價:</p>
 <p style="color:blue">燁聯2015/7月份盤價:</p>
 <p style="color:blue">燁聯2015/7月份盤價:/<p>
 <p style="color:blue">華文專業鋼鐵網台灣地區一週鋼市(TWD/TON):</p>
@@ -82,55 +107,16 @@ message = """<html>
 
 
 </body>
-</html>"""%(date,copper,al,nick,zinc)
+</html>
 
-
+"""
 
 
 f.write(message)
+f.write(message1)
+f.write(message2)
+f.write(message3)
 f.close()
 
 
 
-#SQLite
-con = lite.connect('daily_price.db')
-with con:
-    cur=con.cursor()
-    sql_action='SELECT Record_Date FROM daily WHERE Record_Date=?'
-    parameters = [date_stamp]
-    for row in con.execute(sql_action,parameters):
-        print "find",row[0]
-        break
-    else:
-        sql_InsertDate = 'INSERT INTO daily(Record_Date) VALUES (?)'
-        con.execute(sql_InsertDate, parameters)
-        
-    #update LME LOGIN DATA
-    sql_LMEaction='SELECT Record_Date FROM daily WHERE Record_Date=?'
-    sql_LMEupdate='UPDATE daily SET LME_Copper=?, LME_AL=?, LME_Nick=?, LME_Zinc=? WHERE Record_Date=?'
-    LME_parameters = [lmeValues[0]]
-    LME_Upparameters = [lmeValues[1],lmeValues[2],lmeValues[3],lmeValues[4],lmeValues[0]]
-    for row in con.execute(sql_LMEaction,LME_parameters):
-        print "find",row[0]
-        con.execute(sql_LMEupdate,LME_Upparameters)
-        break
-    else:
-        sql_LME_InsertDate = 'INSERT INTO daily(Record_Date) VALUES (?)'
-        con.execute(sql_LME_InsertDate, LME_parameters)
-        con.execute(sql_LMEupdate,LME_Upparameters)
-    
-    #update ShangHai Copper Price
-    sql_SHaction='SELECT Record_Date FROM daily WHERE Record_Date=?'
-    sql_SHupdate='UPDATE daily SET Shang_Hai_Copper=? WHERE Record_Date=?'
-    SH_parameters = [shValues[0]]
-    SH_Upparameters = [shValues[1], shValues[0]]
-    for row in con.execute(sql_SHaction, SH_parameters):
-        print "find",row[0]
-        con.execute(sql_SHupdate,SH_Upparameters)
-        break
-    else:
-        sql_SH_InsertDate = 'INSERT INTO daily(Record_Date) VALUES (?)'
-        con.execute(sql_SH_InsertDate, SH_parameters)
-        con.execute(sql_SHupdate,SH_Upparameters)
-    
-        
