a2930200ca70bda9886fea5cb31ef5f2588e74c9
Geek-Ragazza <tecnologia.ragazza@gmail.com>
2016-01-06 10:12:13 +0800

    Delete get_interface.py

--- python_internet/get_interface.py
+++
@@ -1,7 +0,0 @@
-from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
-import netifaces as ni
-MyInterface = ni.interfaces()
-print MyInterface
-ni.ifaddresses('eth0')[AF_LINK]
-ni.ifaddresses('eth0')[AF_INET]
-ni.ifaddresses('eth0')[AF_INET][0]['addr']
